08cfd201919b39568dd32e2c0f973550796f91c9
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2024-10-25 11:11:34 +0100

    - CHG: Layout fixes. - CHG: Changed router mode to hash.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -22,23 +22,23 @@
           <v-card
             class="py-4"
             color="primary"
-            :prepend-icon="mdiRocketLaunchOutline"
             rounded="lg"
             variant="tonal"
           >
-            <template #append>
-              <v-avatar image="/img/sebastian.jpg" size="160" />
-            </template>
+            <div class="d-flex flex-column flex-sm-row flex-no-wrap justify-space-between align-center align-sm-start">
+              <div class="order-1 order-sm-0">
+                <v-card-item :prepend-icon="mdiRocketLaunchOutline">
+                  <v-card-title class="text-h5">
+                    Welcome
+                  </v-card-title>
 
-            <template #title>
-              <h2 class="text-h5 font-weight-bold">Welcome</h2>
-            </template>
-
-            <template #subtitle>
-              <div class="text-subtitle-1 text-wrap">
-                Thanks for visiting my home page. Have a look around to see the kind of work I do both professionally and as a hobby.
+                  <v-card-subtitle class="text-wrap">
+                    Thanks for visiting my home page. Have a look around to see the kind of work I do both professionally and as a hobby.
+                  </v-card-subtitle>
+                </v-card-item>
               </div>
-            </template>
+              <v-avatar class="ma-3 order-0 order-sm-1" image="/img/sebastian.jpg" size="160" />
+            </div>
           </v-card>
         </v-col>
 
@@ -46,9 +46,11 @@
           <v-card
             class="py-4"
             rounded="lg"
-            subtitle="Learn more about me and the work I do"
             title="About me"
           >
+            <template #subtitle>
+              <span class="text-wrap">Learn more about me and the work I do</span>
+            </template>
             <v-card-text class="text-wrap">
               <p class="mb-2">I am a Research Software Engineer (RSE) working as part of the Information & Computational Sciences (ICS) Department at the James Hutton Institute. I am responsible for the conceptualisation, management and development of cutting-edge mobile applications, databases and web applications for funders as well as commercial breeding companies. My role is to drive the development of software solutions for plant genetic resources collections, phenotypic data collection and visualization tools.</p>
               <p>In my spare time I like to develop tools for my personal use while trying to create them in a generic way to ensure their utility for others.</p>
@@ -59,45 +61,51 @@
           </v-card>
         </v-col>
 
-        <v-col cols="6">
+        <v-col cols="12" sm="6">
           <v-card
             class="py-4"
             rounded="lg"
-            subtitle="Explore a selection of my projects"
             title="Portfolio"
             to="portfolio"
           >
+            <template #subtitle>
+              <span class="text-wrap">Explore a selection of my projects</span>
+            </template>
             <template #prepend>
               <v-icon color="portfolio" :icon="mdiDeveloperBoard" />
             </template>
           </v-card>
         </v-col>
 
-        <v-col cols="6">
+        <v-col cols="12" sm="6">
           <v-card
             class="py-4"
             rounded="lg"
-            subtitle="See a list of my publications"
             title="Publications"
             to="publications"
           >
+            <template #subtitle>
+              <span class="text-wrap">See a list of my publications</span>
+            </template>
             <template #prepend>
               <v-icon color="publications" :icon="mdiNewspaperVariantMultiple" />
             </template>
           </v-card>
         </v-col>
 
-        <v-col cols="6">
+        <v-col cols="12" sm="6">
           <v-card
             :append-icon="mdiOpenInNew"
             class="py-4"
             href="https://github.com/sebastian-raubach"
             rel="noopener noreferrer"
             rounded="lg"
-            subtitle="See my open-source work on GitHub"
             target="_blank"
             title="GitHub Profile"
           >
+            <template #subtitle>
+              <span class="text-wrap">See my open-source work on GitHub</span>
+            </template>
             <template #prepend>
               <v-icon color="github" :icon="mdiGithub" />
             </template>
@@ -109,7 +117,7 @@
 </template>
 
 <script setup lang="ts">
-  import { mdiGithub, mdiOpenInNew, mdiDeveloperBoard, mdiRocketLaunchOutline, mdiNewspaperVariantMultiple, mdiInformation } from '@mdi/js'
+  import { mdiGithub, mdiOpenInNew, mdiDeveloperBoard, mdiNewspaperVariantMultiple, mdiRocketLaunchOutline, mdiInformation } from '@mdi/js'
 </script>
 
 <style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,12 +6,12 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHashHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: setupLayouts(routes),
 })
 
